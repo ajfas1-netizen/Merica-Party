403b7949e041f474ce099c152d8d8f7e5e5e1b91
ajfas1-netizen <ajfas1@gmail.com>
2026-06-03 21:33:32 -0400

    Add files via upload

--- data.js
+++ data.js
@@ -37,7 +37,7 @@ window.PARTY = {
 
   facts: [
     { icon: "\uD83C\uDF7B", label: "WHEN",  value: "Sun July 5th \u2022 12PM\u20135PM" },
-    { icon: "\uD83C\uDFE1", label: "WHERE", value: "The Fasano Compound \u2022 address texted to confirmed patriots" },
+    { icon: "\uD83C\uDFE1", label: "WHERE", value: "Crane Creek in Martin Downs" },
     { icon: "\uD83E\uDDE2", label: "DRESS", value: "Red, white & questionable \u2022 trashy encouraged, never required" }
   ],
 
